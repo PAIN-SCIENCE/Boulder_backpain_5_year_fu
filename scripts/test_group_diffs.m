481259831a7f3cc844b22bf526e254d71771b7e1
Yoni Ashar <yoniashar@gmail.com>
2024-08-14 11:06:57 -0600

    modelling code

--- scripts/test_group_diffs.m
+++ scripts/test_group_diffs.m
@@ -4,6 +4,25 @@ clear all
 basedir =  '/Users/yoni/Repositories/Boulder_backpain_5_year_fu';
 fiveyr = readtable(fullfile(basedir,'data',"5YearsFollowUp deidentified reIDed.csv"));
 
-%% model outcomes -- need to think through!
+%% test group diffs on pain intensity at 5 yr
 
-model_olp4cbp_outcomes_at5yr('pain_avg', fiveyr)
+outcome = 'pain_avg';
+test_group_diffs_at_5yr(outcome, fiveyr)
+
+%% test for group diffs at 5 years, controlling for baseline
+% I am not using the MEM b/c I would need to convert from wide to long
+% also a MEM seems less apt when I want to test for effects at a specific
+% timepoint (5 years), controlling for baseline. I don't see why I would
+% control for other timepoints (eg 1 year)
+
+function test_group_diffs_at_5yr(outcome, fiveyr)
+
+bl = [outcome '_baseline'];
+
+% contrast coding
+fiveyr.PRT_vs_combined = fiveyr.group == 1;
+fiveyr.PLA_vs_UC = .5 * (fiveyr.group == 2) + -.5 * (fiveyr.group == 3);
+
+fitglm(fiveyr, [outcome ' ~ PRT_vs_combined + PLA_vs_UC + ' bl])
+
+end
