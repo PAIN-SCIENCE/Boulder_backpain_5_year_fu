b6db14cd06eb6375cf3cf66ddc09bc806abbb65f
Yoni Ashar <yoniashar@gmail.com>
2024-12-05 10:26:54 -0700

    fixed reference coding

--- scripts/test_group_diffs_v2.m
+++ scripts/test_group_diffs_v2.m
@@ -6,28 +6,32 @@ fiveyr = readtable(fullfile(basedir,'data',"five_yr_long_final.csv"));
 
 %% test group diffs on pain intensity at 5 yr
 clc
-outcome = 'odi';
+outcome = 'pain_avg';
 [mdl1, mdl2] = test_group_diffs_at_5yr(outcome, fiveyr)
 
 %% Outcomes Table: for each outcome, show M (SD) for each group and test of group diffs
 clc
-fprintf('outcome\tPRT\tPLA\tUC\tPRTvsCom\tp\tPLAvsUC\tp\n')
-for i = [3]% 21:35]
+fprintf('outcome\tPRT\tPLA\tUC\tPRTvsPLA\tp\tPLAvsUC\tp\n')
+for i = 3:15
     outcome_name = fiveyr.Properties.VariableNames{i};
-    outcome = fiveyr{:,outcome_name};
-    var1 = outcome(wh1); var2 = outcome(wh2); var3 = outcome(wh3);
+    
+    % prep for M,SD per group
+    var1 = fiveyr{fiveyr.time==1 & fiveyr.group==1,outcome_name};
+    var2 = fiveyr{fiveyr.time==1 & fiveyr.group==2,outcome_name};
+    var3 = fiveyr{fiveyr.time==1 & fiveyr.group==3,outcome_name};
 
     % if we have baseline data
     if i < 30
-        mdl = test_group_diffs_at_5yr(outcome_name, fiveyr);
+        [mdl1, mdl2] = test_group_diffs_at_5yr(outcome_name, fiveyr);
     
         fprintf(['%s\t%3.2f (%3.2f)\t%3.2f (%3.2f)\t%3.2f (%3.2f)\t' ...
             '%3.2f\t%0.3f\t%3.2f\t%0.3f\n'], ...
         outcome_name, nanmean(var1), nanstd(var1), nanmean(var2), nanstd(var2), nanmean(var3), nanstd(var3), ...
-        mdl.Coefficients.Estimate(3), mdl.Coefficients.pValue(3), mdl.Coefficients.Estimate(4), mdl.Coefficients.pValue(4));
+        mdl1.Coefficients.Estimate(6), mdl1.Coefficients.pValue(6), mdl2.Coefficients.Estimate(6), mdl2.Coefficients.pValue(6));
         %gOLPvsTAU.hedgesg, asts3, gOLPvsTAU.hedgesgCi, bayesF, bayesInt)
     
     else % no baseline data
+        warning('no baseline?')
         mdl = test_group_diffs_at_5yr_nobl(outcome_name, fiveyr);
     
         fprintf(['%s\t%3.2f (%3.2f)\t%3.2f (%3.2f)\t%3.2f (%3.2f)\t' ...
@@ -42,11 +46,21 @@ end
 %% MEM testing for group diffs at 5 years, controlling for baseline
 function [mdl1, mdl2] = test_group_diffs_at_5yr(outcome, fiveyr)
 
-% contrast coding
-fiveyr.PRT_vs_PLA = .5 * (fiveyr.group == 1) + -.5 * (fiveyr.group == 2);
-fiveyr.PRT_vs_UC = .5 * (fiveyr.group == 1) + -.5 * (fiveyr.group == 3);
+% use reference (dummy) coding, as we have a meaningful reference point:
+% pre-treatment for time; control group for group. Group gets automatically
+% dummy coded in fitlme.
 
-mdl1 = fitlme(fiveyr, [outcome ' ~ time*PRT_vs_UC + age + gender + (1|id)']);
-mdl2 = fitlme(fiveyr, [outcome ' ~ time*PRT_vs_PLA + age + gender +(1|id)']);
+% reference code time to 0/1 for pre/post
+fiveyr.time = fiveyr.time > 0;
+
+mdl1 = fitlme(fiveyr, [outcome ' ~ time*group + age + gender +(1|id)'], 'Exclude', fiveyr.group==3);
+mdl2 = fitlme(fiveyr, [outcome ' ~ time*group + age + gender + (1|id)'], 'Exclude', fiveyr.group==2);
+
+% old; contrast coding
+%fiveyr.PRT_vs_PLA = .5 * (fiveyr.group == 1) + -.5 * (fiveyr.group == 2);
+%fiveyr.PRT_vs_UC = .5 * (fiveyr.group == 1) + -.5 * (fiveyr.group == 3);
+% old
+% fiveyr.time = (fiveyr.time / 2) + .5; % to go from -1/1 to -.5/.5
+%fiveyr.PRT_vs_PLA = fiveyr.PRT_vs_PLA + .5;
 
 end
