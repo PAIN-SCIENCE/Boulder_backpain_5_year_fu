c241b68fc6548def1b0f81c93b3ec792e634801a
Yoni Ashar <yoniashar@gmail.com>
2025-03-27 10:22:42 -0600

    figure for completer analyses

--- scripts/plot_full_trajectories.m
+++ scripts/plot_full_trajectories.m
@@ -13,19 +13,14 @@ colors = [0 0 1; .5 0 .5; .5 .5 .5];
 figdir = fullfile(basedir,'figs');
 
 %% drop 6 Ps w/ missing baseline
-nobaseline = setdiff(fiveyr.id, outcomes_wide.id);
-fiveyr = fiveyr(~ismember(fiveyr.id, nobaseline),:);
+%nobaseline = setdiff(fiveyr.id, outcomes_wide.id);
+%fiveyr = fiveyr(~ismember(fiveyr.id, nobaseline),:);
 fiveyr.Properties.VariableNames{2} = 'pain_avg_5yr'; 
 
 %% merge
 fullfive = outerjoin(outcomes_wide, fiveyr, 'Keys', 'id');
 
-wh1 = fullfive.group==1;
-wh2 = fullfive.group==2;
-wh3 = fullfive.group==3;
-
 %% plot
-
 create_figure('main outcome by group')
 
 timepoints = strcat('pain_avg_', {'baseline' 't2_arm_1' 'x1_month_follow_up_arm_1' 'x2_month_follow_up_arm_1' 'x3_month_follow_up_arm_1' 'x6_month_follow_up_arm_1' 'x12_month_follow_up_arm_1', '5yr'});
@@ -46,6 +41,17 @@ ylabel('Pain intensity (0 - 10)'), xlim([xvals(1) xvals(end)])
 ax = gca;
 ax.XTickLabelRotation = 40;
 
+wh1 = fullfive.group==1;
+wh2 = fullfive.group==2;
+wh3 = fullfive.group==3;
+
+% to show only people who are completers at 5 years, uncomment these lines
+%completer_5yr = ~isnan(fullfive.pain_avg_5yr);
+%wh1 = wh1 & completer_5yr;
+%wh2 = wh2 & completer_5yr;
+%wh3 = wh3 & completer_5yr;
+% end optional code block
+
 h1 = lineplot_columns(table2array(fullfive(wh1, timepoints)), 'shade', 'x', xvals, 'color', colors(1,:));
 h2 = lineplot_columns(table2array(fullfive(wh2, timepoints)), 'shade', 'x', xvals, 'color', colors(2,:));
 h3 = lineplot_columns(table2array(fullfive(wh3, timepoints)), 'shade', 'x', xvals, 'color', colors(3,:));
@@ -54,3 +60,4 @@ legend([h1.line_han h2.line_han h3.line_han],{'PRT' 'Placebo' 'Usual Care'}, 'Lo
 set(gcf, 'Position', [     399   164   563   391])
 
 print(gcf, fullfile(figdir, 'pain_trajectories.pdf'), '-dpdf');
+%print(gcf, fullfile(figdir, 'pain_trajectories_completers.pdf'), '-dpdf');
