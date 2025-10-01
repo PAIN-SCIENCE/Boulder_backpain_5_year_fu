01945e0becf9c2d1f1cfe1b8204e7af7358ca5c0
Yoni Ashar <yoniashar@gmail.com>
2025-10-01 10:46:23 -0600

    final tweaks

--- scripts/descriptive_data.m
+++ scripts/descriptive_data.m
@@ -15,7 +15,7 @@ obs = histc(fiveyr.group, 1:3)'
 % Expected frequencies for an equal distribution
 expected = height(fiveyr) / 3 * ones(1, 3);  % Since there are three categories
 
-%% gof
+%% sig group diffs in follow up rates?
 [h,p,stats] = chi2gof(fiveyr.group, 'expected', expected)
 
 %% manual calculation of chi2
@@ -29,3 +29,12 @@ df = height(fiveyr) - 1;
 % Calculate p-value using Chi-Squared cumulative distribution function
 p_value = 1 - chi2cdf(chi2stat, df)
 
+%% age and sex of sample
+
+fiveyr_long = readtable(fullfile(basedir,'data',"five_yr_long_final.csv"));
+fiveyr_long_T1 = fiveyr_long(fiveyr_long.time==1,:);
+
+mean(fiveyr_long_T1.age)
+std(fiveyr_long_T1.age)
+
+histc(fiveyr_long_T1.gender, 0:3)
